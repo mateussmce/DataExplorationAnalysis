73976c691cf842d6919ad4fefeecffc76ceae6dc
mateussmce <mateussm340@gmail.com>
2026-04-12 13:23:39 -0300

    chore(eda-2026-1): remove Python data test file

--- EDA_Assignment1_2026_1/python/src/question_list/data.py
+++
@@ -1,7 +0,0 @@
-questions = [
-    ("Defina Estatística"),
-    ("O que é população?"),
-    ("O que é amostra?"),
-    ("Explique estatística descritiva"),
-    ("Explique estatística inferencial")
-]
